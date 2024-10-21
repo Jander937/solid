e0cd84758f1ef5e3c3ef7fc5ef6be399b319ed42
Jander937 <janderburitica123@gmail.com>
2024-10-20 20:23:51 -0500

    solid principles

---
+++ NBAH/src/main/java/com/example/NBAH/NbahApplication.java
@@ -0,0 +1,13 @@
+package com.example.NBAH;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbahApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NbahApplication.class, args);
+	}
+
+}
---
+++ NBAH/src/test/java/com/example/NBAH/NbahApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.NBAH;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NbahApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
